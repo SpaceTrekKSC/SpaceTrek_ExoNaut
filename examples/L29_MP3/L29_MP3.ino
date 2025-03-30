0f4e34ba5e840b10ae071980f716cb45105da936
RyanSpaceTrek <ryan.bori@spacetrek.com>
2025-03-30 12:06:22 -0400

    Update L29_MP3.ino

--- examples/L29_MP3/L29_MP3.ino
+++ examples/L29_MP3/L29_MP3.ino
@@ -1,6 +1,6 @@
 /*
- * ExoNaut_MP3_Example.ino
- * 
+ * L11_MP3_Playback.ino
+ *
  * Example sketch for demonstrating the ExoNaut_MP3 library
  * with the Space Trek ExoNaut robot.
  * 
@@ -10,8 +10,54 @@
  * - LED indicators for playback status
  * 
  * For Hiwonder MP3 Module (I2C communication)
- * 
- * Date: March 3rd, 2025
+ *
+ * Author: Ryan Bori
+ * Email: ryan.bori@spacetrek.com
+ * Date: March 3, 2025
+ *
+ * Commands:
+ * exonaut robot;                        //This command creates the main robot instance
+ *                                       //This is the object that handles motors and core functions
+ *
+ * ExoNaut_MP3 mp3;                      //This command creates an MP3 player object called 'mp3'
+ *                                       //This is the object that handles audio playback control
+ *
+ * mp3.begin();                          //This command initializes the MP3 module and sets up I2C communication
+ *                                       //Also detects the total number of tracks available
+ *
+ * mp3.setVolume(volume);                //This command sets the playback volume (0-30)
+ *                                       //0 = mute, 30 = maximum volume
+ *
+ * mp3.getTotalTracks();                 //This command returns the total number of tracks detected on the module
+ *
+ * mp3.playTrack(trackNumber);           //This command plays a specific track by number
+ *                                       //Tracks are numbered starting from 1
+ *
+ * mp3.play();                           //This command starts or resumes playback of the current track
+ *
+ * mp3.pause();                          //This command pauses the current playback
+ *
+ * mp3.next();                           //This command skips to the next track
+ *
+ * mp3.previous();                       //This command returns to the previous track
+ *
+ * mp3.getVolume();                      //This command returns the current volume setting
+ *
+ * mp3.volumeUp();                       //This command increases the volume by one step
+ *
+ * mp3.volumeDown();                     //This command decreases the volume by one step
+ *
+ * mp3.getCurrentTrack();                //This command returns the current track number
+ *
+ * robot.setColor(index, r, g, b);       //This command sets the color of an individual LED
+ *                                       //Parameters: LED index, red, green, blue values (0-255)
+ *
+ * robot.setColorAll(r, g, b);           //This command sets all LEDs to the same color
+ *                                       //Parameters: red, green, blue values (0-255)
+ *
+ * robot.clear();                        //This command turns off all LEDs
+ *
+ * robot.show();                         //This command updates the physical LED display
  */
 
 #include <ExoNaut.h>
