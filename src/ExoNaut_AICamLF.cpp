a8291e398bb05819661f41dbfa82a589d4672652
RyanSpaceTrek <ryan.bori@spacetrek.com>
2025-04-26 14:05:38 -0400

    Implemented more into the library to resuce user experience

--- src/ExoNaut_AICamLF.cpp
+++ src/ExoNaut_AICamLF.cpp
@@ -1,293 +1,262 @@
 /*
  * ExoNaut_AICamLF.cpp
  *
- * Author: Based on Andrew Gafford's ExoNaut library
- * Date: March 12, 2025
+ * Author:Andrew Gafford
+ * Updated: April 2025 (Simplified Line Following Functions)
  *
  * Implementation of the ExoNaut_AICamLF class for the Space Trek
  * ExoNaut Robot.
  */
 
-#include "ExoNaut_AICamLF.h"
+ #include "ExoNaut_AICamLF.h"
 
-ExoNaut_AICamLF::ExoNaut_AICamLF() : _robot(nullptr), _camera(nullptr), _initialized(false)
-{
-    // Initialize last line status array to LINE_STATUS_NONE
-    for (int i = 0; i <= MAX_LINE_IDS; i++)
-    {
-        _lastLineStatus[i] = LINE_STATUS_NONE;
-    }
-}
-
-bool ExoNaut_AICamLF::begin(exonaut *robot, ExoNaut_AICam *camera)
-{
-    if (robot == nullptr || camera == nullptr)
-    {
-        return false;
-    }
-
-    _robot = robot;
-    _camera = camera;
-
-    // Check if camera is available
-    char firmwareVersion[16];
-    if (!_camera->firmwareVersion(firmwareVersion))
-    {
-        return false;
-    }
-
-    // Set camera to line following mode
-    if (!_camera->changeFunc(APPLICATION_LINEFOLLOW))
-    {
-        return false;
-    }
-
-    // Turn on the camera LED
-    _camera->setLed(WONDERCAM_LED_ON);
-
-    _initialized = true;
-    return true;
-}
-
-bool ExoNaut_AICamLF::update()
-{
-    if (!_initialized)
-    {
-        return false;
-    }
-
-    // Update camera results
-    _camera->updateResult();
-    return true;
-}
-
-uint8_t ExoNaut_AICamLF::getLineStatus(uint8_t lineId)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS)
-    {
-        return LINE_STATUS_NONE;
-    }
-
-    // Check if the specified line is detected
-    if (_camera->lineIdDetected(lineId))
-    {
-        WonderCamLineResult lineData;
-        if (_camera->lineId(lineId, &lineData))
-        {
-            // Update line status based on offset
-            if (abs(lineData.offset) < LINE_FOLLOW_THRESHOLD)
-            {
-                _lastLineStatus[lineId] = LINE_STATUS_CENTERED;
-            }
-            else if (lineData.offset < 0)
-            {
-                _lastLineStatus[lineId] = LINE_STATUS_LEFT;
-            }
-            else
-            {
-                _lastLineStatus[lineId] = LINE_STATUS_RIGHT;
-            }
-        }
-    }
-    else if (_lastLineStatus[lineId] != LINE_STATUS_NONE)
-    {
-        // Line was detected before but now it's lost
-        _lastLineStatus[lineId] = LINE_STATUS_LOST;
-    }
-
-    return _lastLineStatus[lineId];
-}
-
-int16_t ExoNaut_AICamLF::getLineAngle(uint8_t lineId)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS)
-    {
-        return 0;
-    }
-
-    WonderCamLineResult lineData;
-    if (_camera->lineIdDetected(lineId) && _camera->lineId(lineId, &lineData))
-    {
-        return lineData.angle;
-    }
-
-    return 0;
-}
-
-int16_t ExoNaut_AICamLF::getLineOffset(uint8_t lineId)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS)
-    {
-        return 0;
-    }
-
-    WonderCamLineResult lineData;
-    if (_camera->lineIdDetected(lineId) && _camera->lineId(lineId, &lineData))
-    {
-        return lineData.offset;
-    }
-
-    return 0;
-}
-
-bool ExoNaut_AICamLF::getLineData(uint8_t lineId, WonderCamLineResult *data)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS || data == nullptr)
-    {
-        return false;
-    }
-
-    return _camera->lineIdDetected(lineId) && _camera->lineId(lineId, data);
-}
-
-void ExoNaut_AICamLF::printLineData(uint8_t lineId)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS)
-    {
-        Serial.println("Line follower not initialized or invalid line ID!");
-        return;
-    }
-
-    if (_camera->lineIdDetected(lineId))
-    {
-        WonderCamLineResult lineData;
-        if (_camera->lineId(lineId, &lineData))
-        {
-            Serial.print("Line ID ");
-            Serial.print(lineId);
-            Serial.println(" data:");
-
-            Serial.print("  Start point: (");
-            Serial.print(lineData.start_x);
-            Serial.print(", ");
-            Serial.print(lineData.start_y);
-            Serial.println(")");
-
-            Serial.print("  End point: (");
-            Serial.print(lineData.end_x);
-            Serial.print(", ");
-            Serial.print(lineData.end_y);
-            Serial.println(")");
-
-            Serial.print("  Angle: ");
-            Serial.print(lineData.angle);
-            Serial.println(" degrees");
-
-            Serial.print("  Offset: ");
-            Serial.println(lineData.offset);
-
-            // Print line status
-            Serial.print("  Status: ");
-            switch (getLineStatus(lineId))
-            {
-            case LINE_STATUS_CENTERED:
-                Serial.println("CENTERED");
-                break;
-            case LINE_STATUS_LEFT:
-                Serial.println("LEFT");
-                break;
-            case LINE_STATUS_RIGHT:
-                Serial.println("RIGHT");
-                break;
-            default:
-                Serial.println("UNKNOWN");
-                break;
-            }
-        }
-        else
-        {
-            Serial.print("Error reading data for Line ID ");
-            Serial.println(lineId);
-        }
-    }
-    else
-    {
-        Serial.print("Line ID ");
-        Serial.print(lineId);
-        Serial.println(" not detected");
-    }
-}
-
-void ExoNaut_AICamLF::printAllLines()
-{
-    if (!_initialized)
-    {
-        Serial.println("Line follower not initialized!");
-        return;
-    }
-
-    uint8_t lineCount = getLineCount();
-    Serial.print("Detected ");
-    Serial.print(lineCount);
-    Serial.println(" line(s)");
-
-    if (lineCount > 0)
-    {
-        // Print data for each detected line
-        for (uint8_t id = 1; id <= MAX_LINE_IDS; id++)
-        {
-            if (_camera->lineIdDetected(id))
-            {
-                printLineData(id);
-            }
-        }
-    }
-}
-
-bool ExoNaut_AICamLF::isLineDetected(uint8_t lineId)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS)
-    {
-        return false;
-    }
-
-    return _camera->lineIdDetected(lineId);
-}
-
-uint8_t ExoNaut_AICamLF::getLineCount()
-{
-    if (!_initialized)
-    {
-        return 0;
-    }
-
-    return _camera->numOfLineDetected();
-}
-
-void ExoNaut_AICamLF::followLine(uint8_t lineId, float baseSpeed, float turnFactor)
-{
-    if (!_initialized || lineId > MAX_LINE_IDS)
-    {
-        return;
-    }
-
-    int16_t offset = getLineOffset(lineId);
-    int16_t angle = getLineAngle(lineId);
-
-    // Calculate motor speeds based on line position
-    float leftSpeed = baseSpeed;
-    float rightSpeed = baseSpeed;
-
-    if (_camera->lineIdDetected(lineId))
-    {
-        // Apply proportional control based on offset and angle
-        float offsetCorrection = (offset / 160.0) * turnFactor;
-        float angleCorrection = (angle / 90.0) * turnFactor * 0.5;
-
-        // Combine corrections
-        float correction = offsetCorrection + angleCorrection;
-
-        // Apply correction to motor speeds
-        leftSpeed += correction;
-        rightSpeed -= correction;
-    }
-    else
-    {
-        // No line detected, stop or perform recovery behavior
-        leftSpeed = 0;
-        rightSpeed = 0;
-    }
-
-    // Set motor speeds
-    _robot->set_motor_speed(leftSpeed, rightSpeed);
-}
+ ExoNaut_AICamLF::ExoNaut_AICamLF() : _robot(nullptr), _camera(nullptr), _initialized(false), _baseSpeed(40.0f)
+ {
+     for (int i = 0; i <= MAX_LINE_IDS; i++)
+     {
+         _lastLineStatus[i] = LINE_STATUS_NONE;
+     }
+ }
+ 
+ bool ExoNaut_AICamLF::begin(exonaut *robot, ExoNaut_AICam *camera)
+ {
+     if (robot == nullptr || camera == nullptr)
+     {
+         return false;
+     }
+ 
+     _robot = robot;
+     _camera = camera;
+ 
+     char firmwareVersion[16];
+     if (!_camera->firmwareVersion(firmwareVersion))
+     {
+         return false;
+     }
+ 
+     if (!_camera->changeFunc(APPLICATION_LINEFOLLOW))
+     {
+         return false;
+     }
+ 
+     _camera->setLed(WONDERCAM_LED_ON);
+ 
+     _initialized = true;
+     return true;
+ }
+ 
+ bool ExoNaut_AICamLF::update()
+ {
+     if (!_initialized)
+     {
+         return false;
+     }
+ 
+     _camera->updateResult();
+     return true;
+ }
+ 
+ uint8_t ExoNaut_AICamLF::getLineStatus(uint8_t lineId)
+ {
+     if (!_initialized || lineId > MAX_LINE_IDS)
+     {
+         return LINE_STATUS_NONE;
+     }
+ 
+     if (_camera->lineIdDetected(lineId))
+     {
+         WonderCamLineResult lineData;
+         if (_camera->lineId(lineId, &lineData))
+         {
+             if (abs(lineData.offset) < LINE_FOLLOW_THRESHOLD)
+             {
+                 _lastLineStatus[lineId] = LINE_STATUS_CENTERED;
+             }
+             else if (lineData.offset < 0)
+             {
+                 _lastLineStatus[lineId] = LINE_STATUS_LEFT;
+             }
+             else
+             {
+                 _lastLineStatus[lineId] = LINE_STATUS_RIGHT;
+             }
+         }
+     }
+     else if (_lastLineStatus[lineId] != LINE_STATUS_NONE)
+     {
+         _lastLineStatus[lineId] = LINE_STATUS_LOST;
+     }
+ 
+     return _lastLineStatus[lineId];
+ }
+ 
+ int16_t ExoNaut_AICamLF::getLineAngle(uint8_t lineId)
+ {
+     if (!_initialized || lineId > MAX_LINE_IDS)
+     {
+         return 0;
+     }
+ 
+     WonderCamLineResult lineData;
+     if (_camera->lineIdDetected(lineId) && _camera->lineId(lineId, &lineData))
+     {
+         return lineData.angle;
+     }
+ 
+     return 0;
+ }
+ 
+ int16_t ExoNaut_AICamLF::getLineOffset(uint8_t lineId)
+ {
+     if (!_initialized || lineId > MAX_LINE_IDS)
+     {
+         return 0;
+     }
+ 
+     WonderCamLineResult lineData;
+     if (_camera->lineIdDetected(lineId) && _camera->lineId(lineId, &lineData))
+     {
+         return lineData.offset;
+     }
+ 
+     return 0;
+ }
+ 
+ bool ExoNaut_AICamLF::getLineData(uint8_t lineId, WonderCamLineResult *data)
+ {
+     if (!_initialized || lineId > MAX_LINE_IDS || data == nullptr)
+     {
+         return false;
+     }
+ 
+     return _camera->lineIdDetected(lineId) && _camera->lineId(lineId, data);
+ }
+ 
+ bool ExoNaut_AICamLF::isLineDetected(uint8_t lineId)
+ {
+     if (!_initialized || lineId > MAX_LINE_IDS)
+     {
+         return false;
+     }
+ 
+     return _camera->lineIdDetected(lineId);
+ }
+ 
+ uint8_t ExoNaut_AICamLF::getLineCount()
+ {
+     if (!_initialized)
+     {
+         return 0;
+     }
+ 
+     return _camera->numOfLineDetected();
+ }
+ 
+ void ExoNaut_AICamLF::followLine(uint8_t lineId, float baseSpeed, float turnFactor)
+ {
+     if (!_initialized || lineId > MAX_LINE_IDS)
+     {
+         return;
+     }
+ 
+     WonderCamLineResult lineData;
+     if (_camera->lineIdDetected(lineId) && _camera->lineId(lineId, &lineData))
+     {
+         float centerX = LINE_FOLLOW_CENTER;
+         float errorStartX = (lineData.start_x - centerX) / centerX;
+         float errorAngle = (float)(lineData.angle) / 90.0f;
+         float correction = (OFFSET_WEIGHT * errorStartX) + (ANGLE_WEIGHT * errorAngle);
+         correction *= turnFactor;
+ 
+         float leftSpeed = baseSpeed + correction;
+         float rightSpeed = baseSpeed - correction;
+ 
+         leftSpeed = constrain(leftSpeed, -100.0f, 100.0f);
+         rightSpeed = constrain(rightSpeed, -100.0f, 100.0f);
+ 
+         _robot->set_motor_speed(leftSpeed, rightSpeed);
+     }
+ }
+ 
+ void ExoNaut_AICamLF::setBaseSpeed(float speed)
+ {
+     _baseSpeed = speed;
+ }
+ 
+ void ExoNaut_AICamLF::simpleFollowLine()
+ {
+     if (!_initialized)
+         return;
+ 
+     static unsigned long lastScan = 0;
+     static unsigned long lostLineTimer = 0;
+     static bool recovering = false;
+     static int lastKnownAngle = 0;
+ 
+     if (millis() - lastScan >= SCAN_INTERVAL_MS)
+     {
+         lastScan = millis();
+         update();
+ 
+         uint8_t lineStatus = getLineStatus(1);
+ 
+         if (lineStatus == LINE_STATUS_CENTERED || lineStatus == LINE_STATUS_LEFT || lineStatus == LINE_STATUS_RIGHT)
+         {
+             WonderCamLineResult lineData;
+             if (getLineData(1, &lineData))
+             {
+                 float centerX = LINE_FOLLOW_CENTER;
+                 float errorStartX = (lineData.start_x - centerX) / centerX;
+                 float errorAngle = (float)(lineData.angle) / 90.0f;
+                 float correction = (OFFSET_WEIGHT * errorStartX) + (ANGLE_WEIGHT * errorAngle);
+                 correction *= TURN_FACTOR;
+ 
+                 float speed = _baseSpeed;
+                 if (abs(lineData.angle) > SHARP_TURN_THRESHOLD)
+                 {
+                     speed = SLOW_SPEED;
+                 }
+ 
+                 float leftSpeed = speed + correction;
+                 float rightSpeed = speed - correction;
+ 
+                 leftSpeed = constrain(leftSpeed, -100.0f, 100.0f);
+                 rightSpeed = constrain(rightSpeed, -100.0f, 100.0f);
+ 
+                 _robot->set_motor_speed(leftSpeed, rightSpeed);
+ 
+                 recovering = false;
+                 lastKnownAngle = lineData.angle;
+             }
+         }
+         else if (lineStatus == LINE_STATUS_LOST)
+         {
+             if (!recovering)
+             {
+                 recovering = true;
+                 lostLineTimer = millis();
+             }
+ 
+             if (millis() - lostLineTimer < LOST_RECOVERY_TIMEOUT)
+             {
+                 if (lastKnownAngle > 0)
+                 {
+                     _robot->set_motor_speed(PIVOT_SPEED, -PIVOT_SPEED);
+                 }
+                 else
+                 {
+                     _robot->set_motor_speed(-PIVOT_SPEED, PIVOT_SPEED);
+                 }
+             }
+             else
+             {
+                 _robot->set_motor_speed(0, 0);
+             }
+         }
+         else
+         {
+             _robot->set_motor_speed(0, 0);
+         }
+     }
+ }
+ 
--- src/ExoNaut_AICamLF.h
+++ src/ExoNaut_AICamLF.h
@@ -1,75 +1,89 @@
 /*
  * ExoNaut_AICamLF.h
  *
- * Author: Based on Andrew Gafford's ExoNaut library
- * Date: March 12, 2025
+ * Author:Andrew Gafford
+ * Date: March 12, 2025 (Updated April 2025 for simplified line following)
  *
  * This library extends the ExoNaut_AICam to provide simplified line following
  * functionality for the Space Trek ExoNaut Robot.
  */
 
-#ifndef EXONAUT_AICAMLF_H
-#define EXONAUT_AICAMLF_H
-
-#include <Arduino.h>
-#include "ExoNaut.h"
-#include "ExoNaut_AICam.h"
-
-// Line detection parameters
-#define MAX_LINE_IDS 4           // Maximum number of line IDs the camera can track
-#define LINE_FOLLOW_CENTER 160   // Center point of the camera view (horizontal)
-#define LINE_FOLLOW_THRESHOLD 20 // Threshold for line offset consideration
-
-// Line status constants
-#define LINE_STATUS_NONE 0     // No line detected
-#define LINE_STATUS_CENTERED 1 // Line is centered
-#define LINE_STATUS_LEFT 2     // Line is to the left
-#define LINE_STATUS_RIGHT 3    // Line is to the right
-#define LINE_STATUS_LOST 4     // Line was detected but now lost
-
-class ExoNaut_AICamLF
-{
-public:
-    ExoNaut_AICamLF();
-
-    // Initialize the line follower
-    bool begin(exonaut *robot, ExoNaut_AICam *camera);
-
-    // Update line detection and get latest data
-    bool update();
-
-    // Get line status (none, centered, left, right, lost)
-    uint8_t getLineStatus(uint8_t lineId = 1);
-
-    // Get line angle (-90 to 90 degrees)
-    int16_t getLineAngle(uint8_t lineId = 1);
-
-    // Get line offset from center (-160 to 160)
-    int16_t getLineOffset(uint8_t lineId = 1);
-
-    // Get the full line data structure
-    bool getLineData(uint8_t lineId, WonderCamLineResult *data);
-
-    // Print line data to Serial monitor
-    void printLineData(uint8_t lineId = 1);
-
-    // Print all detected lines to Serial monitor
-    void printAllLines();
-
-    // Check if specific line ID is detected
-    bool isLineDetected(uint8_t lineId = 1);
-
-    // Get number of lines detected
-    uint8_t getLineCount();
-
-    // Motor control helpers based on line position
-    void followLine(uint8_t lineId, float baseSpeed, float turnFactor);
-
-private:
-    exonaut *_robot;
-    ExoNaut_AICam *_camera;
-    bool _initialized;
-    uint8_t _lastLineStatus[MAX_LINE_IDS + 1];
-};
-
-#endif // EXONAUT_AICAMLF_H
+ #ifndef EXONAUT_AICAMLF_H
+ #define EXONAUT_AICAMLF_H
+ 
+ #include <Arduino.h>
+ #include "ExoNaut.h"
+ #include "ExoNaut_AICam.h"
+ 
+ // Line detection parameters
+ #define MAX_LINE_IDS 4           // Maximum number of line IDs the camera can track
+ #define LINE_FOLLOW_CENTER 160   // Center point of the camera view (horizontal)
+ #define LINE_FOLLOW_THRESHOLD 20 // Threshold for line offset consideration
+ 
+ // Line status constants
+ #define LINE_STATUS_NONE 0     // No line detected
+ #define LINE_STATUS_CENTERED 1 // Line is centered
+ #define LINE_STATUS_LEFT 2     // Line is to the left
+ #define LINE_STATUS_RIGHT 3    // Line is to the right
+ #define LINE_STATUS_LOST 4     // Line was detected but now lost
+ 
+ // Internal tuning constants
+ #define SCAN_INTERVAL_MS 20        // Time between scans
+ #define TURN_FACTOR 20.0f          // Turn strength
+ #define ANGLE_WEIGHT 0.5f          // Angle influence weight
+ #define OFFSET_WEIGHT 1.0f         // Start X influence weight
+ #define SHARP_TURN_THRESHOLD 45    // Degrees beyond which slow down
+ #define SLOW_SPEED 20.0f           // Slower speed for sharp turns
+ #define PIVOT_SPEED 20.0f          // Speed for pivot recovery
+ #define LOST_RECOVERY_TIMEOUT 1000 // Max ms to try pivoting
+ 
+ class ExoNaut_AICamLF
+ {
+ public:
+     ExoNaut_AICamLF();
+ 
+     // Initialize the line follower
+     bool begin(exonaut *robot, ExoNaut_AICam *camera);
+ 
+     // Update line detection and get latest data
+     bool update();
+ 
+     // Get line status (none, centered, left, right, lost)
+     uint8_t getLineStatus(uint8_t lineId = 1);
+ 
+     // Get line angle (-90 to 90 degrees)
+     int16_t getLineAngle(uint8_t lineId = 1);
+ 
+     // Get line offset from center (-160 to 160)
+     int16_t getLineOffset(uint8_t lineId = 1);
+ 
+     // Get the full line data structure
+     bool getLineData(uint8_t lineId, WonderCamLineResult *data);
+ 
+     // Check if specific line ID is detected
+     bool isLineDetected(uint8_t lineId = 1);
+ 
+     // Get number of lines detected
+     uint8_t getLineCount();
+ 
+     // Advanced manual control helper
+     void followLine(uint8_t lineId, float baseSpeed, float turnFactor);
+ 
+     // --- Simplified functions for beginners ---
+ 
+     // Set base driving speed
+     void setBaseSpeed(float speed);
+ 
+     // Simple line follower with auto turn, slow down, recovery
+     void simpleFollowLine();
+ 
+ private:
+     exonaut *_robot;
+     ExoNaut_AICam *_camera;
+     bool _initialized;
+     uint8_t _lastLineStatus[MAX_LINE_IDS + 1];
+     float _baseSpeed;
+ };
+ 
+ #endif // EXONAUT_AICAMLF_H
+ 
